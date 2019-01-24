9da3357add8f0da4a6cfb1a512c27b02bdfd4768
swift_gan <swift_gan@trendmicro.com.cn>
2019-01-24 11:03:22 +0800

    kill inline

--- app/src/main/java/com/swift/sandhook/test/ActivityHooker.java
+++ app/src/main/java/com/swift/sandhook/test/ActivityHooker.java
@@ -24,25 +24,27 @@ public class ActivityHooker {
     @HookMethod("onCreate")
     @MethodParams(Bundle.class)
     public static void onCreate(Activity thiz, Bundle bundle) {
-        Log.e("ActivityHooker", "hooked onCreate success " + thiz);
+        Log.e("ActivityHooker", "hooked onCreate success " + thiz.hashCode());
         onCreateBackup(thiz, bundle);
     }
 
     @HookMethodBackup("onCreate")
     @MethodParams(Bundle.class)
     public static void onCreateBackup(Activity thiz, Bundle bundle) {
-
+        //invoke self to kill inline
+        onCreateBackup(thiz, bundle);
     }
 
     @HookMethod("onPause")
     public static void onPause(Activity thiz) {
-        Log.e("ActivityHooker", "hooked onPause success " + thiz);
+        Log.e("ActivityHooker", "hooked onPause success " + thiz.hashCode());
         onPauseBackup(thiz);
     }
 
     @HookMethodBackup("onPause")
     public static void onPauseBackup(Activity thiz) {
-
+        //invoke self to kill inline
+        onPauseBackup(thiz);
     }
 
 }
