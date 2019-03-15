0edd30877ac952ac4d3a6d35524475cc0b059afc
swift_gan <swift_gan@trendmicro.com.cn>
2019-03-15 11:11:23 +0800

    add pass reflection on Android Q

---
+++ hooklib/src/main/java/com/swift/sandhook/utils/ReflectionUtils.java
@@ -0,0 +1,38 @@
+package com.swift.sandhook.utils;
+
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+//
+// Created by Swift Gan on 2019/3/15.
+//
+
+public class ReflectionUtils {
+
+    public static Method forNameMethod;
+    public static Method getMethodMethod;
+
+    static Class vmRuntimeClass;
+    static Method addWhiteListMethod;
+
+    static Object vmRuntime;
+
+    static {
+        try {
+            getMethodMethod = Class.class.getDeclaredMethod("getDeclaredMethod", String.class, Class[].class);
+            forNameMethod = Class.class.getDeclaredMethod("forName", String.class);
+            vmRuntimeClass = (Class) forNameMethod.invoke(null, "dalvik.system.VMRuntime");
+            addWhiteListMethod = (Method) getMethodMethod.invoke(vmRuntimeClass, "setHiddenApiExemptions", new Class[]{String[].class});
+            Method getVMRuntimeMethod = (Method) getMethodMethod.invoke(vmRuntimeClass, "getRuntime", null);
+            vmRuntime = getVMRuntimeMethod.invoke(null);
+        } catch (Exception e) {
+            Log.e("ReflectionUtils", "error get methods", e);
+        }
+    }
+
+    //className like Lcom/swift/sandhook/utils/ReflectionUtils
+    public static void addReflectionWhiteList(String... classNames) throws Throwable {
+        addWhiteListMethod.invoke(vmRuntime, new Object[] {classNames});
+    }
+}
