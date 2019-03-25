1e683c1c6be8aa9874b2fa0eeb41b21bf3ea3772
swift_gan <swift_gan@trendmicro.com.cn>
2019-03-25 15:40:53 +0800

    tweak HookBlackList

--- hooklib/src/main/java/com/swift/sandhook/blacklist/HookBlackList.java
+++ hooklib/src/main/java/com/swift/sandhook/blacklist/HookBlackList.java
@@ -1,25 +1,35 @@
 package com.swift.sandhook.blacklist;
 
 import java.lang.reflect.Member;
+import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HookBlackList {
 
-    static Set<String> blackList = new HashSet<>();
+    public static Set<String> methodBlackList = new HashSet<>();
+    public static Set<Class> classBlackList = new HashSet<>();
 
     static {
-        blackList.add("java.lang.reflect.Member.getDeclaringClass");
-        blackList.add("java.lang.reflect.Method.invoke");
-        blackList.add("java.util.Map.get");
-        blackList.add("java.lang.reflect.AccessibleObject.setAccessible");
-        blackList.add("java.lang.reflect.Member.getModifiers");
-        blackList.add("java.lang.reflect.InvocationTargetException.getCause");
+        methodBlackList.add("java.lang.reflect.Member.getDeclaringClass");
+        methodBlackList.add("java.lang.reflect.Method.invoke");
+        methodBlackList.add("java.util.Map.get");
+        methodBlackList.add("java.util.concurrent.ConcurrentHashMap.get");
+        methodBlackList.add("java.lang.reflect.AccessibleObject.setAccessible");
+        methodBlackList.add("java.lang.reflect.Member.getModifiers");
+        methodBlackList.add("java.lang.reflect.InvocationTargetException.getCause");
+        methodBlackList.add("java.lang.reflect.Method.hashCode");
+        methodBlackList.add("java.lang.reflect.Class.getName");
+        methodBlackList.add("java.lang.String.hashCode");
+        methodBlackList.add("java.lang.String.length");
     }
 
     public final static boolean canNotHook(Member origin) {
+        if (classBlackList.contains(origin.getDeclaringClass()))
+            return true;
         String name = origin.getDeclaringClass().getName() + "." + origin.getName();
-        return blackList.contains(name);
+        return methodBlackList.contains(name);
     }
 
 }
