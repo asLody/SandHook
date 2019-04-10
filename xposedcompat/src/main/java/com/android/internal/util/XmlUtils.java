6f69e00a4c3b5c1860c1869fc6f384516bfacbfa
swift_gan <swift_gan@trendmicro.com.cn>
2019-04-10 22:18:33 +0800

    fix XSharePreference

---
+++ xposedcompat/src/main/java/com/android/internal/util/XmlUtils.java
@@ -0,0 +1,14 @@
+package com.android.internal.util;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.InputStream;
+import java.util.Map;
+
+public class XmlUtils {
+
+    public static Map readMapXml(InputStream inputStream) throws XmlPullParserException {
+        return null;
+    }
+
+}
--- xposedcompat/src/main/java/de/robv/android/xposed/XSharedPreferences.java
+++ xposedcompat/src/main/java/de/robv/android/xposed/XSharedPreferences.java
@@ -5,11 +5,20 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.android.internal.util.XmlUtils;
+
+import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+
+import de.robv.android.xposed.services.FileResult;
 
 /**
  * This class is basically the same as SharedPreferencesImpl from AOSP, but
@@ -69,7 +78,13 @@ public final class XSharedPreferences implements SharedPreferences {
 	 */
 	@SuppressLint("SetWorldReadable")
 	public boolean makeWorldReadable() {
-		return false;
+		if (!SELinuxHelper.getAppDataFileService().hasDirectFileAccess())
+			return false; // It doesn't make much sense to make the file readable if we wouldn't be able to access it anyway.
+
+		if (!mFile.exists()) // Just in case - the file should never be created if it doesn't exist.
+			return false;
+
+		return mFile.setReadable(true, false);
 	}
 
 	/**
@@ -97,7 +112,47 @@ public final class XSharedPreferences implements SharedPreferences {
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private void loadFromDiskLocked() {
+		if (mLoaded) {
+			return;
+		}
 
+		Map map = null;
+		FileResult result = null;
+		try {
+			result = SELinuxHelper.getAppDataFileService().getFileInputStream(mFilename, mFileSize, mLastModified);
+			if (result.stream != null) {
+				map = XmlUtils.readMapXml(result.stream);
+				result.stream.close();
+			} else {
+				// The file is unchanged, keep the current values
+				map = mMap;
+			}
+		} catch (XmlPullParserException e) {
+			Log.w(TAG, "getSharedPreferences", e);
+		} catch (FileNotFoundException ignored) {
+			// SharedPreferencesImpl has a canRead() check, so it doesn't log anything in case the file doesn't exist
+		} catch (IOException e) {
+			Log.w(TAG, "getSharedPreferences", e);
+		} finally {
+			if (result != null && result.stream != null) {
+				try {
+					result.stream.close();
+				} catch (RuntimeException rethrown) {
+					throw rethrown;
+				} catch (Exception ignored) {
+				}
+			}
+		}
+
+		mLoaded = true;
+		if (map != null) {
+			mMap = map;
+			mLastModified = result.mtime;
+			mFileSize = result.size;
+		} else {
+			mMap = new HashMap<>();
+		}
+		notifyAll();
 	}
 
 	/**
@@ -116,7 +171,16 @@ public final class XSharedPreferences implements SharedPreferences {
 	 * <p><strong>Warning:</strong> With enforcing SELinux, this call might be quite expensive.
 	 */
 	public synchronized boolean hasFileChanged() {
-		return false;
+		try {
+			FileResult result = SELinuxHelper.getAppDataFileService().statFile(mFilename);
+			return mLastModified != result.mtime || mFileSize != result.size;
+		} catch (FileNotFoundException ignored) {
+			// SharedPreferencesImpl doesn't log anything in case the file doesn't exist
+			return true;
+		} catch (IOException e) {
+			Log.w(TAG, "hasFileChanged", e);
+			return true;
+		}
 	}
 
 	private void awaitLoadedLocked() {
