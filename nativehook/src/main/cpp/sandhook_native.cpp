8039b52cb3a8d2699946dae938eccb9d205501a2
LoveSy <shana@zju.edu.cn>
2021-04-03 22:45:20 +0800

    +r before hook

--- nativehook/src/main/cpp/sandhook_native.cpp
+++ nativehook/src/main/cpp/sandhook_native.cpp
@@ -8,9 +8,22 @@
 #include "sandhook_native.h"
 #include "hook.h"
 #include "elf.h"
+#include <sys/mman.h>
 
 using namespace SandHook::Hook;
 using namespace SandHook::Elf;
+
+#define _uintval(p)               reinterpret_cast<uintptr_t>(p)
+#define _ptr(p)                   reinterpret_cast<void *>(p)
+#define _align_up(x, n)           (((x) + ((n) - 1)) & ~((n) - 1))
+#define _align_down(x, n)         ((x) & -(n))
+#define _page_size                4096
+#define _page_align(n)            _align_up(static_cast<uintptr_t>(n), _page_size)
+#define _ptr_align(x)             _ptr(_align_down(reinterpret_cast<uintptr_t>(x), _page_size))
+#define _make_rwx(p, n)           ::mprotect(_ptr_align(p), \
+                                              _page_align(_uintval(p) + n) != _page_align(_uintval(p)) ? _page_align(n) + _page_size : _page_align(n), \
+                                              PROT_READ | PROT_WRITE | PROT_EXEC)
+
 
 extern "C"
 EXPORT void* SandGetModuleBase(const char* so) {
@@ -25,6 +38,7 @@ EXPORT void* SandGetSym(const char* so, const char* symb) {
 
 extern "C"
 EXPORT void* SandInlineHook(void* origin, void* replace) {
+    _make_rwx(origin, _page_size);
     return InlineHook::instance->Hook(origin, replace);
 }
 
